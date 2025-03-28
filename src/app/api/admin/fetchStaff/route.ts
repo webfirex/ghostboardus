3fde39c5c6478db952ef0462ba7115118a056b72
Prakhar Vohra <webfirex@gmail.com>
2025-03-29 04:45:18 +0530

    Updates

--- src/app/api/admin/fetchStaff/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import db from "@/lib/db";
-
-export async function GET() {
-    const staff = await db.query("SELECT * FROM admins");
-
-  return NextResponse.json({ staff: staff.rows });
-}
--- src/app/api/admin/fetchTopWatch/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import db from "@/lib/db";
-
-export async function GET() {
-    const topwatch = await db.query("SELECT * FROM topwatch");
-
-  return NextResponse.json({ totalStocks: topwatch.rows.length, topwatch: topwatch.rows });
-}
