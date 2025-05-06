e3a8d828241848971413bec783cd0ac51a47aa44
Prakhar Vohra <webfirex@gmail.com>
2025-05-06 23:23:05 +0530

    Udpates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
                 t.src=v;s=b.getElementsByTagName(e)[0];
                 s.parentNode.insertBefore(t,s)}(window, document,'script',
                 'https://connect.facebook.net/en_US/fbevents.js');
-                fbq('init', '2197070084040958');
+                fbq('init', '1399119927787536');
                 fbq('track', 'PageView');
               `,
             }}
@@ -58,7 +58,7 @@ export default function RootLayout({
               height="1"
               width="1"
               style={{ display: 'none' }}
-              src="https://www.facebook.com/tr?id=2197070084040958&ev=PageView&noscript=1"
+              src="https://www.facebook.com/tr?id=1399119927787536&ev=PageView&noscript=1"
             />
           </noscript>
       </head>
